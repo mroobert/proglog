7ad4e1391d87e3cc77a97ae0d3c270c393b5a37b
mroobert <robertmihairo94@gmail.com>
2022-05-05 09:41:19 +0300

    JSON over HTTP Server

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/mroobert/proglog/internal/server"
+	"log"
+)
+
+func main() {
+	srv := server.NewHttpServer(":8080")
+	log.Fatal(srv.ListenAndServe())
+}
---
+++ internal/server/http.go
@@ -0,0 +1,90 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewHttpServer(addr string) *http.Server {
+	handler := newHandler()
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler.handleProduce).Methods(http.MethodPost)
+	r.HandleFunc("/", handler.handleConsume).Methods(http.MethodGet)
+
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
+
+type handler struct {
+	Log *Log
+}
+
+func newHandler() *handler {
+	return &handler{
+		Log: NewLog(),
+	}
+}
+
+func (h *handler) handleProduce(w http.ResponseWriter, r *http.Request) {
+	var req ProduceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	off, err := h.Log.Append(req.Record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) handleConsume(w http.ResponseWriter, r *http.Request) {
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	record, err := h.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res := ConsumeResponse{Record: record}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
